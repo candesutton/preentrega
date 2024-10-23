d7ec3b160d74c3ad9f94eee1a052726a8c16d1f8
candesutton <candesutton@gmail.com>
2024-10-23 08:57:16 -0300

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import { Krona_One } from "next/font/google";
+import {Montserrat} from "next/font/google";
 import "./globals.css";
 
 const inter1 = Roboto({ subsets: ["latin"], weight: '400' }); 
 const inter2 = Krona_One({ subsets: ["latin"], weight: '400' }); 
+const inter3 = Montserrat({subsets: ["latin"], weight: ['400', '700']});
 
 
 export const metadata: Metadata = {
@@ -19,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter1.className}>{children}</body>
+      <body className={inter3.className}>{children}</body>
     </html>
   );
 
--- app/page.tsx
+++ app/page.tsx
@@ -5,58 +5,85 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen items-start justify-start p-24 bg-white">
+    <main className="flex flex-col min-h-screen p-24 bg-white">
       
-      <h6 className="text-8xl mt-40 mb-60 text-black text-left self-start">
-  El <strong className="font-bold">cambio climático</strong> y<br /> 
-  la amenaza frente<br />
-  a las ciudades costeras
-</h6>
-
-
-      <h5 className="text-2xl mt-60 mb-0 text-black font-bold self-start">Paleta de colores</h5>
-      <img src="/losquefaltan-03.svg" alt="Visualización 03" className="w-[400px] h-auto mb-0 mt-0" />
-
-
-      <h1 className="text-2xl mt-20 mb-0 text-black font-bold self-start">Cantidad según proyección de crecimiento de nivel de mar</h1>
-
-      <img src="/losquefaltan-04.svg" alt="Visualización 04" className="w-[300px] h-auto mb-0 mt-0" />
-
-
-      <h2 className="text-2xl  mt-20 mb-0 text-black font-bold self-start">Impacto huella de carbono en la ciudad (más círculos más impacto)</h2>
-
-      <img src="/losquefaltan-05.svg" alt="Visualización 05" className="w-[600px] h-auto mb-0 mt-0" />
-
- 
-      <h3 className="text-2xl  mt-20 mb-0 text-black font-bold self-start">Qué tan grande es la amenaza de inundación en esa ciudad <br /> 
-      (más anillos más amenaza)<br />
-      </h3>
-      <img src="/losquefaltan-06.svg" alt="Visualización 06" className="w-[300px] h-auto mb-0 mt-0" />
-
-      <p className="text-2xl mt-20 mb-0 text-black font-bold self-start">
-    Proyección de área terrenal de una ciudad costera a través de los años
-      </p>
-
-
-      <div className="flex flex-row space-x-4">
-      <img src="/losquevan-11.svg" alt="Visualización 11" className="w-[250px] h-auto mb-0 mt-8" />
-      <img src="/losquevan-12.svg" alt="Visualización 12" className="w-[250px] h-auto mb-0 mt-8" />
-      <img src="/losquevan-13.svg" alt="Visualización 13" className="w-[250px] h-auto mb-0 mt-8" />
+      <div className="relative w-full h-auto">
+      <img src="/Portada-03.svg" alt="Visualización 00" className="w-full h-auto mb-0 mt-0" />
+      <p className="absolute top-8 left-1/2 transform -translate-x-1/2 text-white text-1xl text-center">LENGUAJE VISUAL</p>
+      <h1 className="text-6xl absolute inset-0 flex flex-col items-center justify-center text-center text-white max-w-xxl mx-auto">
+      EL IMPACTO DEL CAMBIO CLIMÁTICO EN LAS<br />
+      <span className="font-bold">CIUDADES COSTERAS</span>
+      </h1>
       </div>
+
+
+   
+    <h1 className="text-4xl mb-0 mt-24 text-black font-bold text-center">PALETA DE COLORES</h1>
+    <img src="/paleta.svg" alt="Visualización 01" className="w-[500px] h-auto mb-0 mt-16 mx-auto" />
+
+
+
+
+
+      <h2 className="text-4xl mt-40 mb-0 text-black font-bold text-center">FORMA PRINCIPAL</h2>
+      <img src="/forma_principal.svg" alt="Visualización 02" className="w-[600px] h-auto mb-0 mt-16 mx-auto" />
+
+
+      <h3 className="text-4xl mt-40 mb-0 text-black font-bold text-center">ANALISIS</h3>
+      <div className="text-black mx-auto text-justify text-center max-w-4xl mt-10 mb-10">
+        <h4 className="text-2xl font-400">
+          LA FORMA ORGÁNICA Y FLUIDA DE ESTA FIGURA, JUNTO CON SU GRADIENTE DE AZUL A BLANCO, EVOCA EL AGUA Y LAS TRANSICIONES NATURALES, LO QUE LA HACE IDEAL PARA REPRESENTAR EL IMPACTO DEL CAMBIO CLIMÁTICO EN LAS CIUDADES COSTERAS. EL CENTRO VACÍO SIMBOLIZA LA PÉRDIDA DE TIERRAS Y ESPACIOS HABITABLES POR EL AUMENTO DEL NIVEL DEL MAR, MIENTRAS QUE LOS FRAGMENTOS ALREDEDOR SUGIEREN EFECTOS SECUNDARIOS COMO LA EROSIÓN Y LA DESTRUCCIÓN DE ECOSISTEMAS. SU ABSTRACCIÓN PERMITE MÚLTIPLES INTERPRETACIONES, GENERANDO UN IMPACTO VISUAL QUE REFLEJA LA COMPLEJIDAD Y LOS EFECTOS DISPERSOS DEL CAMBIO CLIMÁTICO.
+        </h4>
+      </div>
+
       
-    
-
-      <h4 className="text-2xl  mt-20 mb-0 text-black font- bold self-start">Ejemplos:</h4>
-
-      <div className="flex flex-row space-x-4">
-      <img src="/losquevan-07.svg" alt="Visualización 07" className="w-[600px] h-auto mb-0 mt-0" />
-      <img src="/losquevan-08.svg" alt="Visualización 08" className="w-[600px] h-auto mb-0 mt-0" />
+      <h5 className="text-4xl mt-40 mb-10 text-black font-bold text-center">+/- TAMAÑO SEGÚN PROYECCIÓN DE CRECIMIENTO DE NIVEL DE MAR</h5>
+      
+      <div className="flex justify-center space-x-24 mt-16">
+        <img src="/forma_principal.svg" alt="Visualización 03" className="w-[300px] h-auto mb-0" />
+        <img src="/forma_principal.svg" alt="Visualización 04" className="w-[400px] h-auto mb-0" />
       </div>
 
-      <div className="flex flex-row space-x-4">
-      <img src="/losquevan-09.svg" alt="Visualización 09" className="w-[600px] h-auto  mb-0 mt-0" />
-      <img src="/losquevan-10.svg" alt="Visualización 10" className="w-[600px] h-auto mb-0 mt-0" />
+
+      <h6 className="text-4xl mt-40 mb-0 text-black font-bold text-center">IMPACTO HUELLA DE CARBONO EN LA CIUDAD<br />(MÁS AROS MÁS IMPACTO)</h6>
+      <div className="flex justify-center space-x-24 mt-16">
+        <img src="/impacto_huella.svg" alt="Visualización 05" className="w-[400px] h-auto mb-10" />
+        <img src="/forma_principal.svg" alt="Visualización 06" className="w-[400px] h-auto mb-10" />
       </div>
+
+
+      <p className="text-4xl mt-40 mb-0 text-black font-bold text-center">IMPACTO HUELLA DE CARBONO EN LA CIUDAD<br />(MÁS FORMAS MÁS IMPACTO)</p>
+      <div className="flex justify-center space-x-24 mt-16">
+        <img src="/impacto_huella_ciudad_2.svg" alt="Visualización 05" className="w-[400px] h-auto mb-10" />
+        <img src="/impacto_huella_ciudad.svg" alt="Visualización 06" className="w-[400px] h-auto mb-10" />
+      </div>
+
+
+      <p className="text-4xl mt-40 mb-0 text-black font-bold text-center">PROYECCIÓN DE ÁREA TERRENAL DE UNA CIUDAD COSTERA <br />
+      (+ ANCHO + PROYECCIÓN)</p>
+      <div className="flex justify-center space-x-24 mt-16">
+        <img src="/visu-07.svg" alt="Visualización 07" className="w-[400px] h-auto mb-10" />
+        <img src="/visu-06.svg" alt="Visualización 08" className="w-[400px] h-auto mb-10" />
+      </div>
+
+      <p className="text-4xl mt-40 mb-0 text-black font-bold text-left">EJEMPLOS DE APLICACIÓN EN CIUDADES</p>
+
+      <div className="flex space-x-4">
+        <div className="flex-1">
+        <p className="text-5xl text-left text-black mt-32">MIAMI</p>
+        <img src="/visu-08.svg" alt="Visualización 08" className="w-[400px] h-auto mt-12 mb-0 ml-48" />
+        <p className="text-5xl text-left text-black mt-32">NUEVA YORK</p>
+        <img src="/visu-10.svg" alt="Visualización 09" className="w-[400px] h-auto mt-16 mb-0 ml-48" />
+        </div>
+        <div className="flex-1">
+        <p className="text-5xl text-left text-black mt-32">DHAKA</p>
+        <img src="/visu-09.svg" alt="Visualización 10" className="w-[400px] h-auto mt-12 mb-0 ml-48" />
+        <p className="text-5xl text-left text-black mt-32">BUENOS AIRES</p>
+        <img src="/visu-11.svg" alt="Visualización 11" className="w-[400px] h-auto mt-16 mb-0 ml-48" />
+      
+        </div>
+      </div>
+
 
     </main>
   );
